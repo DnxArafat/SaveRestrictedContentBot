bb2c7da72f7d9abe1a534ff4706a9f185c7161d0
DnxArafat <51267324+DnxArafat@users.noreply.github.com>
2024-09-10 16:40:05 +0600

    Update and rename __init__.py to new__init__.py

--- main/new__init__.py
+++ main/new__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("24266722", default=None, cast=int)
+API_HASH = config("5dd6bedd030fd733bb5997a1d62a9b2a", default=None)
+BOT_TOKEN = config("7531297522:AAGHKt2RYE2GLnTocUnxrNNSNsdU-woMQhM", default=None)
+SESSION = config("BQFyR-IAPg0pJs56iu2ummr_hjfiILPXHPey5RbIh4_whFE-J8ZYlfEmaWqATeX3PmzszFl2jNzOh0EN3VYxYudXLY-c35ilvekJOVEIjTwyGWNzONURrqSrXte1sUwXF1PdVorWu7Zt9Ka1i-0ZtwOhctLYLF0sDpmItYRq1PMN316QSf73yyJCoTJ6P-vy0fHLymUW63b4blFBISfjKhWlLbjPjG5hzGztzh9KJGkWTiDK_TZxZkr50s31Ps5bcdSfZ6T8xiaiTDvn2tpOccOCO1Olj5vgRfuxk-M-k9MjHZzJk7QpoEGJCMTrrEp4F-S14KL6o2kfwtcZy_SvMUyxTbHfIgAAAAGm6WZPAA", default=None)
+FORCESUB = config("BinaryPaidLeaker", default=None)
+AUTH = config("5091918803", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
